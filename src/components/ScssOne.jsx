b75e13b919bdcf37e450ad1582ea64809ce4561a
QTSYQ <50006931+QTSYQ@users.noreply.github.com>
2022-10-29 19:27:08 +0900

    first commit
    dd

---
+++ src/components/ScssOne.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function ScssOne() {
+  return (
+    <>
+      <div className="gridBox">
+        <span>숙제</span>
+        <span></span>
+        <span className="menu">Menu</span>
+        <div className="numberRow">
+          <div className="number">#1</div>
+          <div className="number">#2</div>
+          <div className="number">#3</div>
+          <div className="number">#4</div>
+          <div className="number">#5</div>
+        </div>
+        <div className="numberRow">
+          <div className="number">#6</div>
+          <div className="number">#7</div>
+          <div className="number">#8</div>
+          <div className="number">#9</div>
+          <div className="number">#10</div>
+        </div>
+        <div className="centerImage"></div>
+        <span></span>
+        <div class="scrollingText">
+          <span>
+            Thirty-four artists visually interpret and countdown their favorite
+            albums of 2019
+          </span>
+        </div>
+        <span></span>
+      </div>
+    </>
+  );
+}
