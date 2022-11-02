2d1a24d8f4c18efa7911ad31e42d5cf5c4baa42a
QTSYQ <50006931+QTSYQ@users.noreply.github.com>
2022-11-03 08:05:38 +0900

    ScssThree 추가
    schwartzmedia

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import ScssOne from "./components/ScssOne/ScssOne";
 import ScssTwo from "./components/SccsTwo/ScssTwo";
+import ScssThree from "./components/ScssThree/ScssThree";
 
 function App() {
   return (
     <>
       {/* <ScssOne></ScssOne> */}
-      <ScssTwo />
+      {/* <ScssTwo /> */}
+      {/* <ScssThree /> */}
     </>
   );
 }
--- src/components/SccsTwo/ScssTwo.jsx
+++ src/components/SccsTwo/ScssTwo.jsx
@@ -4,8 +4,8 @@ export default function ScssTwo() {
   return (
     <>
       <header>
-        <div class="header__left">
-          <div class="logo">
+        <div className="header__left">
+          <div className="logo">
             <span>zoo</span>
           </div>
           <div>
@@ -14,7 +14,7 @@ export default function ScssTwo() {
             <span>clients</span>
           </div>
         </div>
-        <div class="header__right">
+        <div className="header__right">
           <div>
             <span>works</span>
             <span>all</span>
@@ -25,113 +25,121 @@ export default function ScssTwo() {
         </div>
       </header>
       <main>
-        <article class="content">
-          <img class="content__img" src="" />
-          <div class="content__title">Courants Verts</div>
+        <article className="content">
+          <img className="content__img" src="" />
+          <div className="content__title">Courants Verts</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="" />
-          <div class="content__title">Château d'écouen</div>
+        <article className="content">
+          <img className="content__img" src="" />
+          <div className="content__title">Château d'écouen</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="" />
-          <div class="content__title">MOCO Montpellier Contemporain</div>
+        <article className="content">
+          <img className="content__img" src="" />
+          <div className="content__title">MOCO Montpellier Contemporain</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="" />
-          <div class="content__title">Amos Sport Business School</div>
+        <article className="content">
+          <img className="content__img" src="" />
+          <div className="content__title">Amos Sport Business School</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="" />
-          <div class="content__title">architectures CREE magazine</div>
+        <article className="content">
+          <img className="content__img" src="" />
+          <div className="content__title">architectures CREE magazine</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="" />
-          <div class="content__title">IDHEAL</div>
+        <article className="content">
+          <img className="content__img" src="" />
+          <div className="content__title">IDHEAL</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="" />
-          <div class="content__title">Rero Via Negativa - Skira</div>
+        <article className="content">
+          <img className="content__img" src="" />
+          <div className="content__title">Rero Via Negativa - Skira</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="" />
-          <div class="content__title">Terabilis Paysage</div>
+        <article className="content">
+          <img className="content__img" src="" />
+          <div className="content__title">Terabilis Paysage</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="" />
-          <div class="content__title">37 Quai Branly, Paris</div>
+        <article className="content">
+          <img className="content__img" src="" />
+          <div className="content__title">37 Quai Branly, Paris</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="" />
-          <div class="content__title">R.Burman photography</div>
+        <article className="content">
+          <img className="content__img" src="" />
+          <div className="content__title">R.Burman photography</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="" />
-          <div class="content__title">La Lettre du Musicien</div>
+        <article className="content">
+          <img className="content__img" src="" />
+          <div className="content__title">La Lettre du Musicien</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="" />
-          <div class="content__title">La Rue - MOCO Panacée</div>
+        <article className="content">
+          <img className="content__img" src="" />
+          <div className="content__title">La Rue - MOCO Panacée</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="" />
-          <div class="content__title">LM éditions</div>
+        <article className="content">
+          <img className="content__img" src="" />
+          <div className="content__title">LM éditions</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="" />
-          <div class="content__title">ZAT 100 artistes dans la ville</div>
+        <article className="content">
+          <img className="content__img" src="" />
+          <div className="content__title">ZAT 100 artistes dans la ville</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="" />
-          <div class="content__title">
+        <article className="content">
+          <img className="content__img" src="" />
+          <div className="content__title">
             Basquiat à la Fondation Louis Vuitton
           </div>
         </article>
-        <article class="content">
-          <img class="content__img" src="./feature/크으으.jpg" />
-          <div class="content__title">
+        <article className="content">
+          <img className="content__img" src="./feature/크으으.jpg" />
+          <div className="content__title">
             Les architectes ouvrent les portes 18
           </div>
         </article>
-        <article class="content">
-          <img class="content__img" src="./feature/크으으.jpg" />
-          <div class="content__title">Millecamps, tapisseries années 60-70</div>
+        <article className="content">
+          <img className="content__img" src="./feature/크으으.jpg" />
+          <div className="content__title">
+            Millecamps, tapisseries années 60-70
+          </div>
         </article>
-        <article class="content">
-          <img class="content__img" src="./feature/크으으.jpg" />
-          <div class="content__title">Choregraphy, We design experience</div>
+        <article className="content">
+          <img className="content__img" src="./feature/크으으.jpg" />
+          <div className="content__title">
+            Choregraphy, We design experience
+          </div>
         </article>
-        <article class="content">
-          <img class="content__img" src="./feature/크으으.jpg" />
-          <div class="content__title">Anton Seder, Fantaisies & Ornements</div>
+        <article className="content">
+          <img className="content__img" src="./feature/크으으.jpg" />
+          <div className="content__title">
+            Anton Seder, Fantaisies & Ornements
+          </div>
         </article>
-        <article class="content">
-          <img class="content__img" src="./feature/크으으.jpg" />
-          <div class="content__title">Catalogue diplômes HEAR 2017</div>
+        <article className="content">
+          <img className="content__img" src="./feature/크으으.jpg" />
+          <div className="content__title">Catalogue diplômes HEAR 2017</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="./feature/크으으.jpg" />
-          <div class="content__title">HEAR DIPLÔMES 17</div>
+        <article className="content">
+          <img className="content__img" src="./feature/크으으.jpg" />
+          <div className="content__title">HEAR DIPLÔMES 17</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="./feature/크으으.jpg" />
-          <div class="content__title">J'aime le Centre Pompidou</div>
+        <article className="content">
+          <img className="content__img" src="./feature/크으으.jpg" />
+          <div className="content__title">J'aime le Centre Pompidou</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="./feature/크으으.jpg" />
-          <div class="content__title">Jardin Monde au Centre Pompidou</div>
+        <article className="content">
+          <img className="content__img" src="./feature/크으으.jpg" />
+          <div className="content__title">Jardin Monde au Centre Pompidou</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="./feature/크으으.jpg" />
-          <div class="content__title">K Architectures</div>
+        <article className="content">
+          <img className="content__img" src="./feature/크으으.jpg" />
+          <div className="content__title">K Architectures</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="./feature/크으으.jpg" />
-          <div class="content__title">MGAU</div>
+        <article className="content">
+          <img className="content__img" src="./feature/크으으.jpg" />
+          <div className="content__title">MGAU</div>
         </article>
-        <article class="content">
-          <img class="content__img" src="./feature/크으으.jpg" />
-          <div class="content__title">Michel Péna, Jouer/Jouir du paysage</div>
+        <article className="content">
+          <img className="content__img" src="./feature/크으으.jpg" />
+          <div className="content__title">
+            Michel Péna, Jouer/Jouir du paysage
+          </div>
         </article>
         <div></div>
       </main>
